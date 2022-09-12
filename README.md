091cb1a28cae813c58ed4fea832195bbcafd076d
Shagun Dwivedi <83537273+ShagunDwivedi@users.noreply.github.com>
2022-09-12 17:46:57 +0000

    Final Commit

--- README.md
+++ README.md
@@ -17,24 +17,29 @@ The project is aimed at creating an app that allows multiple users to create tra
 # Folder Structure
 
 - `trackerdb`  is the sqlite database.
-- `madapp` is where our application code is.
-- `setup.sh` set up the virtualenv inside a local `.env` folder. Uses `pyproject.toml` and `poetry` to setup the project.
-- `local_run.sh`  Used to run the flask application in development mode.
+- `madapp` is where the code for the app is.
+- `setup.sh` sets up the virtualenv inside a local `.env` folder. Uses `pyproject.toml` and `poetry` to setup the project.
+- `local_run.sh`  is used to run the flask application in development mode.
 - `static` - default `static` files folder. It serves at '/static' path, and has images required .
 - `templates` - Default flask templates folder.
+- `app_apis.yaml` has the API documentation.
 
 
 ```
 ├── local_run.sh
 ├── local_setup.sh
 ├── madapp
+│   ├── api.py
 │   ├── controller.py
 │   ├── database.py
+│   ├── for_security.py
 │   ├── models.py
 │   ├── plotforapp.py
 │   └── __pycache__
+|       ├── api.cpython-310.pyc
 │       ├── controller.cpython-310.pyc
 │       ├── database.cpython-310.pyc
+|       ├── for_security.cpython-310.pyc
 │       ├── models.cpython-310.pyc
 │       └── plotforapp.cpython-310.pyc
 ├── main.py
@@ -44,13 +49,15 @@ The project is aimed at creating an app that allows multiple users to create tra
 │   ├── minimalism1.gif
 │   └── plot.jpg
 ├── templates
+|   ├── security
+|   |   ├── _formhelpers.html
+|   |   ├── login_user.html
+│   |   └── register_user.html
 │   ├── addlog.html
 │   ├── addtracker.html
 │   ├── dash.html
 │   ├── homepage.html
-│   ├── login.html
 │   ├── logs.html
-│   ├── signup.html
 │   ├── updatelog.html
 │   └── updatetracker.html
 └── trackerdb.sqlite3
--- madapp/controller.py
+++ madapp/controller.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,request,redirect
 from flask import current_app as app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
+from flask_security import login_required
+from flask_login import current_user
 import os
 from datetime import datetime
 from .plotforapp import *
@@ -9,52 +11,23 @@ from .database import db
 from .models import *
 
 
-
+def get_uname():
+    user1 = current_user
+    return user1.id
 
 @app.route("/",methods = ["GET", "POST"])
 def home():    
     return(render_template("homepage.html"))
 
 
-@app.route("/signup",methods = ["GET","POST"])
-def signup():
-    if(request.method == "GET"):
-        return(render_template("signup.html",err="Pick a Username"))
-    elif(request.method == "POST"):
-        req = request.form
-        fname = req.get("fname")
-        uname = req.get("uname")
-        #redirect to the errorpage if already exists
-        if(len(user.query.filter_by(user_id=uname).all())!=0):
-           return(render_template("signup.html",err="Username Already Exists, Pick another Username!"))
-        #but before that take in the data into the database
-        us = user(user_id=uname,name=fname)
-        db.session.add(us)
-        db.session.commit()
-        #redirect to  dashboard
-        return(redirect("/"+uname+"/dashboard"))
-
-
-@app.route("/login",methods = ["GET","POST"])
-def login():
-    if(request.method == "GET"):
-        return(render_template("login.html",err="Welcome Back!"))
-    elif(request.method == "POST"):
-        req = request.form
-        uname = req.get("uname")
-        #check if account exists
-        if(len(user.query.filter_by(user_id=uname).all())==0):
-            return(render_template("login.html",err="No Such Username Exists"))
-        #if no account send no account exists message
-        #else to dashboard
-        us = user.query.filter_by(user_id=uname).first()
-        return(redirect("/"+uname+"/dashboard"))
-
-
-@app.route("/<string:uname>/dashboard",methods = ["GET","POST"])
-def dashboard(uname):
+@app.route("/dashboard",methods = ["GET","POST"])
+@login_required
+def dashboard():
     #find username in db
-    user1 = db.session.query(user).filter_by(user_id=uname).first()
+    uname = get_uname()
+    #print(uname)
+
+    #user1 = db.session.query(user).filter_by(user_id=uname).first()
     #link to tracker table
     trackerlist = db.session.query(tracker).filter_by(user_id=uname).all()
     #get the last log out of all the trackers
@@ -70,12 +43,15 @@ def dashboard(uname):
                     logdic[trkr]=[z[-1]]
             #take tracker.name into there
     #print(logdic[trackerlist[0]][0].note)
-    return(render_template("dash.html",uname=user1.name,userid=user1.user_id, logdic=logdic, trackerlist=trackerlist))
+    return(render_template("dash.html",uname=current_user.name,userid=uname, logdic=logdic, trackerlist=trackerlist))
 
 
-@app.route("/<string:uname>/<int:trackerid>")
-def trackerlog(uname,trackerid):
+@app.route("/<int:trackerid>")
+@login_required
+def trackerlog(trackerid):
     #get tracker name
+    uname = get_uname()
+
     trakr = db.session.query(tracker).filter_by(trk_id=trackerid).first()
 
     #log list where tracker id = tracker
@@ -85,8 +61,10 @@ def trackerlog(uname,trackerid):
     return(render_template("logs.html",userid=uname, tracker=trakr, loglist=loglist))
 
 
-@app.route("/<string:uname>/addtracker",methods = ["GET","POST"])
-def addtracker(uname):
+@app.route("/addtracker",methods = ["GET","POST"])
+@login_required
+def addtracker():
+    uname = get_uname()
     if(request.method == "GET"):
         return(render_template("addtracker.html", userid=uname))
     elif(request.method == "POST"):
@@ -112,47 +90,65 @@ def addtracker(uname):
                     mcq = multiplechoice(trk_id=track.trk_id, value=x)
                     db.session.add(mcq)
                     db.session.commit()
-        #add to tracker table(maybe outsource to a function)
+        #add to tracker table
         #redirect to that tracker
-        return(redirect("/"+uname+"/"+str(track.trk_id)))
+        return(redirect("/"+str(track.trk_id)))
     
 
-@app.route("/<string:uname>/<int:trackerid>/delete")
-def deletetracker(uname,trackerid):
+@app.route("/<int:trackerid>/delete")
+@login_required
+def deletetracker(trackerid):
     db.session.query(tracker).filter_by(trk_id=trackerid).delete()
     db.session.query(log).filter_by(trk_id=trackerid).delete()
     db.session.query(multiplechoice).filter_by(trk_id=trackerid).delete()
     db.session.commit()
-    return(redirect("/"+uname+"/dashboard"))
+    return(redirect("/dashboard"))
 
     
-@app.route("/<string:uname>/<int:trackid>/update",methods = ["GET","POST"])
-def updatetracker(uname,trackid):
+@app.route("/<int:trackid>/update",methods = ["GET","POST"])
+@login_required
+def updatetracker(trackid):
+    uname = get_uname()
     if(request.method=="GET"):
         track = db.session.query(tracker).filter_by(trk_id=trackid).first()
-        return(render_template("updatetracker.html",tracker=track, userid=uname))
+        track_type = db.session.query(trak_type).all()
+        return(render_template("updatetracker.html",tracker=track, userid=uname, track_type=track_type))
     elif(request.method=="POST"):
-        #still need to update
+        #still need to update 
         req = request.form
         name = req.get("trakname")
         desc = req.get("trakdesc")
+        trk_type = req.get("trak_type")
         trac = db.session.query(tracker).filter_by(trk_id=trackid).first()
         trac.trk_name = name
         trac.description = desc
+        trac.trk_type = trk_type
         db.session.commit()
-        return(redirect("/"+uname+"/dashboard"))
+        if(trk_type=="3"):
+            db.session.query(multiplechoice).filter_by(trk_id=trackid).delete()
+            setting = req.get("settings")
+            z = setting.split(",")
+            for x in z:
+                if(x!=''):
+                    mcq = multiplechoice(trk_id=trackid, value=x)
+                    db.session.add(mcq)
+                    db.session.commit()
+        db.session.query(log).filter(log.trk_id==trackid).delete()
+        db.session.commit()
+        return(redirect("/dashboard"))
 
 
-@app.route("/<string:uname>/<int:trackid>/addlog",methods = ["GET","POST"])
-def addlog(uname,trackid):
+@app.route("/<int:trackid>/addlog",methods = ["GET","POST"])
+@login_required
+def addlog(trackid):
+    uname = get_uname()
     track = db.session.query(tracker).filter_by(trk_id=trackid).first()
     if(request.method=="GET"):
         value=[]
         if(track.trk_type==3):
             value=db.session.query(multiplechoice).filter_by(trk_id=trackid).all()
-        return(render_template("addlog.html",track=track, values=value, userid=uname))
+        return(render_template("addlog.html",track=track, values=value))
     elif(request.method=="POST"):
-        tracktype = track.trk_type
         req = request.form
         #tracker
         #user
@@ -160,41 +156,38 @@ def addlog(uname,trackid):
         #note
         note = req.get("message")
         val = req.get("value")
-        if(val == "Other"):
-          newmcqval = req.get("other")
-          mcq = multiplechoice(trk_id=trackid, value=newmcqval)
-          val = newmcqval
-          db.session.add(mcq)
-          db.session.commit()
         lg = log(trk_id=trackid, user_id=uname, value=val, time=func.now(), note=note)
         db.session.add(lg)
         db.session.commit()
-        return(redirect("/"+uname+"/"+str(trackid)))
+        return(redirect("/"+str(trackid)))
 
     
-@app.route("/<string:uname>/<int:trackid>/<timestamp>/deletelog")
-def deletelog(uname,trackid,timestamp):
-    db.session.query(log).filter((log.trk_id==trackid) & (log.time==timestamp)).delete()
+@app.route("/<int:trackid>/<int:logid>/deletelog")
+@login_required
+def deletelog(trackid,logid):
+    db.session.query(log).filter((log.trk_id==trackid) & (log.log_id==logid)).delete()
     db.session.commit()
-    return(redirect("/"+uname+"/"+str(trackid)))
+    return(redirect("/"+str(trackid)))
     
 
     
-@app.route("/<string:uname>/<int:trackid>/<timestamp>/updatelog", methods = ["GET","POST"])
-def updatelog(uname,trackid,timestamp):
+@app.route("/<int:trackid>/<int:logid>/updatelog", methods = ["GET","POST"])
+@login_required
+def updatelog(trackid,logid):
+    uname = get_uname()
     if(request.method == "GET"):
-        lg = db.session.query(log).filter((log.trk_id==trackid) & (log.time==timestamp)).first()
+        lg = db.session.query(log).filter((log.trk_id==trackid) & (log.log_id==logid)).first()
         track = db.session.query(tracker).filter_by(trk_id=trackid).first()
         value=[]
         if(track.trk_type==3):
             value=db.session.query(multiplechoice).filter_by(trk_id=trackid).all()
         prevvalue = lg.value
         prevnote = lg.note
-        return(render_template("updatelog.html",userid=uname, values=value, prevvalue=prevvalue, prevnote=prevnote, timestamp=timestamp, trackid=trackid, track=track))
+        return(render_template("updatelog.html",userid=uname, values=value, prevvalue=prevvalue, prevnote=prevnote, logid=logid, trackid=trackid, track=track))
     elif(request.method == "POST"):
         req = request.form
         val = req.get("value")
         note = req.get("message")
-        db.session.query(log).filter((log.trk_id==trackid) & (log.time==timestamp)).update({'value': val, 'note':note})
+        db.session.query(log).filter((log.trk_id==trackid) & (log.log_id==logid)).update({'value': val, 'note':note})
         db.session.commit()
-        return(redirect("/"+uname+"/"+str(trackid)))
+        return(redirect("/"+str(trackid)))
